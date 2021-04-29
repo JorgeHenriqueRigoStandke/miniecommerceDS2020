fce07cafdbc92861597c461987775df9a3f0b58c
JorjaaoDaMassa <79273493+JorjaaoDaMassa@users.noreply.github.com>
2021-04-29 14:47:26 -0300

    Source Packages

---
+++ src/java/controller/managebeans/PaginasMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.managebeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author jorge
+ */
+@Named(value = "paginasMB")
+@RequestScoped
+public class PaginasMB {
+
+    /**
+     * Creates a new instance of PaginasMB
+     */
+    public PaginasMB() {
+    }
+    
+}
