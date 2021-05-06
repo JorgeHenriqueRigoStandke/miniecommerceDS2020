6e6f5c478f7da29e26bdc13ecfbf20508fc668a8
JorjaaoDaMassa <79273493+JorjaaoDaMassa@users.noreply.github.com>
2021-05-06 17:22:00 -0300

    verificador de senhas

--- src/java/controller/managebeans/UsuarioB.java
+++ src/java/controller/managebeans/UsuarioB.java
@@ -32,7 +32,7 @@ public class UsuarioB {
     
     public void salvarCadastro()
     {
-        
+        if(senha.equals(confirmarSenha)){
         Usuario u = new Usuario();
         
         u.setNome(nome);
@@ -53,6 +53,11 @@ public class UsuarioB {
         {
             FacesContext context = FacesContext.getCurrentInstance();
             context.addMessage(null, new FacesMessage (FacesMessage.SEVERITY_INFO, "O Cliente " + nome + " foi cadastrado com sucesso!", ""));
+        }
+        }
+        else{
+           FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage (FacesMessage.SEVERITY_INFO, "As senhas não são iguais!", "")); 
         }
     }
     
